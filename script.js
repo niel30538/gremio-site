2ac61fec7da57440fa99e4f228e02062f4171b53
João Emanuel Silva Viana <joaoesvianaa@gmail.com>
2025-11-22 21:55:13 -0300

    Ajustes

--- script.js
+++ script.js
@@ -4,11 +4,9 @@
 const menuToggle = document.getElementById("menu-toggle");
 const navbar = document.querySelector(".navbar");
 
-if (menuToggle && navbar) {
-  menuToggle.addEventListener("click", () => {
+menuToggle.addEventListener("click", () => {
     navbar.classList.toggle("active");
-  });
-}
+});
 
 /* -----------------
       CARROSSEL (somente se existir)
